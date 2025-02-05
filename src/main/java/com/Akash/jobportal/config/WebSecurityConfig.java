374c93834e54ec4cc533ba3e3ad3ecc2d380126c
AkashSinghfeb21 <akashsinghfeb21@gmail.com>
2025-02-05 20:11:16 +0530

    05-02-2025 evening

--- src/main/java/com/Akash/jobportal/config/WebSecurityConfig.java
+++ src/main/java/com/Akash/jobportal/config/WebSecurityConfig.java
@@ -1,13 +1,29 @@
 package com.Akash.jobportal.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+
+import com.Akash.jobportal.services.CustomUserDetailsService;
 
 @Configuration
 public class WebSecurityConfig {
 	
+	private final CustomUserDetailsService customUserDetailsService;
+	
+	
+	@Autowired
+	public WebSecurityConfig(CustomUserDetailsService customUserDetailsService) {
+
+		this.customUserDetailsService = customUserDetailsService;
+	}
+
 	//list of public urls which doesn't require login
 	private final String[] publicUrl = {"/",
 			"/global-search/**",
@@ -28,11 +44,31 @@ public class WebSecurityConfig {
 	@Bean
 	protected SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
 		
+		http.authenticationProvider(authenticationProvider());
+		
 		http.authorizeHttpRequests(auth->{
 			auth.requestMatchers(publicUrl).permitAll();//this doesn't need authentication
 			auth.anyRequest().authenticated();//this need authentication
 		});
 		
 		return http.build();
+	}
+	
+	@Bean
+	public AuthenticationProvider authenticationProvider() {
+		
+		DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+		
+		authenticationProvider.setPasswordEncoder(passwordEncoder());
+		
+		authenticationProvider.setUserDetailsService(customUserDetailsService);
+	    
+		return authenticationProvider;
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		
+		return new BCryptPasswordEncoder();
 	}
 }
