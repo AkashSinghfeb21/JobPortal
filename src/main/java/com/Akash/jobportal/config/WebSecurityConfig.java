5c7827bc5f9542d92222b4b07288f600b8ce683d
AkashSinghfeb21 <akashsinghfeb21@gmail.com>
2025-02-14 11:49:44 +0530

    14-02-2025

--- src/main/java/com/Akash/jobportal/config/WebSecurityConfig.java
+++ src/main/java/com/Akash/jobportal/config/WebSecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -16,12 +17,14 @@ import com.Akash.jobportal.services.CustomUserDetailsService;
 public class WebSecurityConfig {
 	
 	private final CustomUserDetailsService customUserDetailsService;
+	private final CustomAuthenticationSuccessHandler customAuthenticationSuccessHandler;
 	
 	
 	@Autowired
-	public WebSecurityConfig(CustomUserDetailsService customUserDetailsService) {
+	public WebSecurityConfig(CustomUserDetailsService customUserDetailsService,CustomAuthenticationSuccessHandler customAuthenticationSuccessHandler) {
 
 		this.customUserDetailsService = customUserDetailsService;
+		this.customAuthenticationSuccessHandler = customAuthenticationSuccessHandler;
 	}
 
 	//list of public urls which doesn't require login
@@ -50,6 +53,14 @@ public class WebSecurityConfig {
 			auth.requestMatchers(publicUrl).permitAll();//this doesn't need authentication
 			auth.anyRequest().authenticated();//this need authentication
 		});
+		
+		http.formLogin(form->form.loginPage("/login").permitAll()
+				.successHandler(customAuthenticationSuccessHandler))
+		     .logout(logout->{
+		    	 logout.logoutUrl("/logout");
+		    	 logout.logoutSuccessUrl("/");
+		     }).cors(Customizer.withDefaults()).
+		     csrf(csrf->csrf.disable());
 		
 		return http.build();
 	}
