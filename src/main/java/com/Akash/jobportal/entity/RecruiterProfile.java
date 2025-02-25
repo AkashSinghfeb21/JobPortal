7a634cf080ab9d18b934e7b3d871bf258ae7f839
AkashSinghfeb21 <akashsinghfeb21@gmail.com>
2025-02-25 13:16:20 +0530

    25-02-2025

--- src/main/java/com/Akash/jobportal/entity/RecruiterProfile.java
+++ src/main/java/com/Akash/jobportal/entity/RecruiterProfile.java
@@ -123,7 +123,7 @@ public class RecruiterProfile {
 	@Transient
 	public String getPhotosImagePath() {
 		if(profilePhoto==null)return null;
-		return "/photos/recruiter/"+userAccountId+"/"+profilePhoto;
+		return "photos/recruiter/"+userAccountId+"/"+profilePhoto;
 	}
 
 	@Override
